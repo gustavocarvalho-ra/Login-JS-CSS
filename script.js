f6f140d679c093d3f260c60566d1fe1cb9e02c9e
GustavoCR021 <gustavoc1922@gmail.com>
2024-02-28 01:39:50 -0300

    at10/f

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const wrapper = document.querySelector('.wrapper')
 const loginLink = document.querySelector('.login-link')
 const registerLink = document.querySelector('.register-link')
+const btnPopup = document.querySelector('.btnLogin-popup')
+const iconClose = document.querySelector('.icon-close')
+
 
 registerLink.addEventListener('click', () => {
   wrapper.classList.add('active');
@@ -8,4 +11,12 @@ registerLink.addEventListener('click', () => {
 
 loginLink.addEventListener('click', () => {
   wrapper.classList.remove('active');
+})
+
+btnPopup.addEventListener('click', () => {
+  wrapper.classList.add('active-popup');
+})
+
+iconClose.addEventListener('click', () => {
+  wrapper.classList.remove('active-popup');
 })
